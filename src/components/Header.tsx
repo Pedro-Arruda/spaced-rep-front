ef8867c08c821c6ede6cf09f22de1b07a06f5345
Pedro Arruda <arruda.pedro15@outlook.com>
2024-03-31 22:47:35 -0300

    adiciona logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,12 @@
+import logoApp from "../assets/SpacedRep.png";
+
 export const Header = () => {
   return (
     <div className="py-5 bg-container ">
-      <nav className="max-w-[1366px] mx-auto px-7 flex gap-10 text-lg font-bold">
-        <a href="/">English</a>
-        <ul className="flex gap-5">
+      <nav className="max-w-[1366px] mx-auto px-7 flex items-center gap-5 text-lg font-bold">
+        {/* <a href="/">English</a> */}
+        <img src={logoApp} alt="" width={170} />
+        <ul className="flex gap-3">
           <li>
             <a className="cursor-pointer" href="/">
               Cards
